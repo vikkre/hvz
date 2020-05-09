c2c4f9185668a68830405b21143d3a8364c3ae5c
Viktor Kretzschmar <viktor.kretzschmar@gmail.com>
2020-05-09 17:39:12 +0200

    add envirenment vaiable for database to docker-compose file

--- api/app.py
+++ api/app.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@database/test'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+database = os.environ["DATABASE"]
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin@database/" + database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
 import products
+
+db.create_all()
 
 @app.route('/')
 def index():
