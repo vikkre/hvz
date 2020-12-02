b8a9da3fa80be3a9032b60f4596d263808d6ffff
Ralf Kretzschmar <ralf.kretzschmar@googlemail.com>
2020-12-02 22:41:00 +0100

    Work on #32 Move docker files to config Create batch file for running all tests. Make e2etests conftest.py wait for database and selenium

--- frontend/test_e2e/conftest.py
+++ frontend/test_e2e/conftest.py
@@ -9,6 +9,32 @@ from .pages import (ProductEditPage, ProductListPage, RecipeEditPage,
                     RecipeListPage, ShoppingListPage, StartPage)
 
 
+def retry(f, extype=Exception, max_count=10, incr_factor=2):
+    """
+    Call the callable f. If an execption of type exttype occurs,
+    sleep a while and retry.
+
+    Retry up to max_count times.
+
+    Multiply the current retry count by incr_factor to obtain the
+    number of seconds to sleep.
+    """
+    retry_count = 0
+    # result = None
+    while True:
+        try:
+            # result = f()
+            return f()
+            # break
+        except extype as e:
+            retry_count += 1
+            if (retry_count > max_count):
+                raise e
+            else:
+                time.sleep(incr_factor * retry_count)
+    # return result
+
+
 @pytest.fixture(scope="session")
 def base_url():
     return "http://hvz_web"
@@ -16,26 +42,14 @@ def base_url():
 
 @pytest.fixture(scope="session")
 def db():
-    retry_count = 0
-    d = None
-    while True:
-        try:
-            d = Database()
-            break
-        except Exception as e:
-            retry_count += 1
-            if (retry_count > 10):
-                raise e
-            else:
-                time.sleep(2 * retry_count)
-    yield d
+    yield retry(lambda: Database())
 
 
 @pytest.fixture(scope="session")
 def browser():
     selenium_host = os.getenv("SELENIUM_HOST", "localhost")
     selenium_url = f"http://{selenium_host}:4444/wd/hub"
-    with Browser(driver_name="remote", command_executor=selenium_url) as b:
+    with retry(lambda: Browser(driver_name="remote", command_executor=selenium_url)) as b:
         b.driver.set_window_size(360, 800)
         yield b
 
